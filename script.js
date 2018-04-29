bf7b6422fab0804b0577312d6ad18a89c7b3c412
Your Namehglottman <hglottman@gmail.com>
2018-04-29 09:24:46 +0300

    individual q with handleBars

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
 
+var $bookDiv = $('.book');
 var fetch = function () {
+    var ISBNnumber = $('#isbn').val();
     $.ajax({
         method: "GET",
-        url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:0439023521',
+        url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + ISBNnumber ,
         success: function (data) {
-            var bookTitle = data.items[0].volumeInfo.title;
-            var bookAuthor = data.items[0].volumeInfo.authors[0];
-            var bookDescription = data.items[0].volumeInfo.description;
-            var bookImage = data.items[0].volumeInfo.imageLinks.smallThumbnail;
-            var $formDiv = $(this).closest('.book-form');
-            var $bookDiv = $($formDiv).siblings('.book');
-            // var bookAdded = '<div> <h2>' + bookTitle + '</h2> <br>' + '<p>' + bookAuthor + '</p> <br>' +
-            //     '<h3> "Written by "' + bookAuthor + '</h3> <br>' +
-            //     '<img src =' + bookImage + 'alt="book Images"> </div>'
-
-            var source = $('.bookToAdd').html();
-            var template = Handlebars.compile(source);
-            var newHTML = template({bookTitle, bookAuthor, bookAuthor, bookImage});
-            // append our new html to the page
-            $bookDiv.append(newHTML);
-            console.log(data);
+            renderBooks(data);
         },
         error: function (jqXHR, textStatus, errorThrown) {
             console.log(textStatus);
@@ -27,9 +14,28 @@ var fetch = function () {
     });
 };
 
+function renderBooks(data){
+    var bookTitle = data.items[0].volumeInfo.title;
+    var bookAuthor = data.items[0].volumeInfo.authors[0];
+    var bookDescription = data.items[0].volumeInfo.description;
+    var bookImage = data.items[0].volumeInfo.imageLinks.smallThumbnail;
+    console.log(bookDescription);
+    // var bookAdded = '<div> <h2>' + bookTitle + '</h2> <br>'
+    //  + '<p>' + bookDescription + '</p> <br>' +
+    //     '<h3> "Written by "' + bookAuthor + '</h3> <br>' +
+    //     '<img src =' + bookImage + 'alt="book Images"> </div>'
+
+    var source = $('#bookToAdd').html();
+    var template = Handlebars.compile(source);
+    var newHTML = template({bookTitle, bookDescription, bookAuthor, bookImage});
+    // append our new html to the page
+    $bookDiv.append(newHTML);
+    console.log(data);
+}
+
 
 $(".search-book").on("click", function () {
-    fetch(this);
+    fetch();
 })
 
 
